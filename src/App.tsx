68251c52e13f27733cc0aac744d8fabc6e87f21c
Andre Oelschlaegel <post@andre-oelschlaegel.de>
2023-08-17 16:27:33 +0200

    Build the colormode  switch

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,12 @@ function App() {
 
     return (
         <>
-            <Grid templateAreas={{
-                base: `"nav" "main"`,
-                lg: `"nav nav" "aside main"`,
-            }}>
-                <GridItem area={'nav'}><NavBar /></GridItem>
+            <Grid templateAreas={{base: `"nav" "main"`, lg: `"nav nav" "aside main"`,}}>
+                <GridItem area='nav'><NavBar /></GridItem>
                 <Show above={'lg'}>
-                    <GridItem area={'aside'} bg={'gold'}>Aside</GridItem>
+                    <GridItem area={'aside'}>Aside</GridItem>
                 </Show>
-                <GridItem area={'main'} bg={'dodgerblue'}>Main</GridItem>
+                <GridItem area={'main'}>Main</GridItem>
             </Grid>
         </>
     )
