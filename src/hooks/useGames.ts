57be760eae8f83d887c20641b8b696315d07faa2
Andre Oelschlaegel <post@andre-oelschlaegel.de>
2023-08-18 15:54:07 +0200

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import {useEffect, useState} from "react";
 import apiClient from "../services/api-client.ts";
 import {CanceledError} from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGameResponse {
@@ -21,11 +28,15 @@ const useGames = () => {
         const controller = new AbortController();
 
         apiClient.get<FetchGameResponse>('/games', {signal: controller.signal})
-            .then(res => setGames(res.data.results))
+            .then(res => {
+                // console.log(res.data.results);
+                setGames(res.data.results)
+            })
             .catch(err => {
                 if (err instanceof CanceledError) return;
                 setError(err.message)
             });
+
         return () => controller.abort();
     }, []);
 
