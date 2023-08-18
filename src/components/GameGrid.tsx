d7ec846e5ad962cd5885c9a9d8e5bdaed10a887e
Andre Oelschlaegel <post@andre-oelschlaegel.de>
2023-08-18 13:52:59 +0200

    Building game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,13 @@
 import useGames from "../hooks/useGames.ts";
+import {GameCard} from "./GameCard.tsx";
+import {SimpleGrid} from "@chakra-ui/react";
 
 export const GameGrid = () => {
   const {games, error}=  useGames();
-    return (
-        <>
-            <ul>
-                {games.map(game => <li key={game.id}>{game.name}</li>)}
-            </ul>
-            {error && <p>{error}</p>}
-        </>
-    );
+    return <>
+        <SimpleGrid columns={{sm:1, md: 2, lg:3, xl:5}} padding='10px' spacing={10}>
+            {games.map(game => <GameCard key={game.id} game={game} />)}
+        </SimpleGrid>
+        {error && <p>{error}</p>}
+    </>;
 };
